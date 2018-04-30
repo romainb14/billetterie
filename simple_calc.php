c8695b1d1fe7248b977ad56889ef703bf3bb64d7
romainb14 <romainbochet14@gmail.com>
2018-04-30 13:52:23 +0000

    1ers calculs

---
+++ simple_calc.php
@@ -0,0 +1,9 @@
+<?php
+$operande1=10;
+$operande2=2;
+
+echo $operande1+$operande2."\n";
+echo ($operande1-$operande2)."\n";
+echo ($operande1*$operande2)."\n";
+echo ($operande1/$operande2)."\n";
+
