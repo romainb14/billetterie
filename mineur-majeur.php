17d01710ae0124303d048167146e28b27d63e889
Vagrant Default User <vagrant@billetterie>
2018-06-13 17:35:09 +0000

    Utilisation de la fonction if

---
+++ mineur-majeur.php
@@ -0,0 +1,25 @@
+<?php
+
+if ($argv[2] >= 18) {
+    echo $argv[1] . " est majeur" . "\n";
+}
+
+if ($argv[2] < 18) {
+    echo $argv[1] . " est mineur" . "\n";
+}
+
+if ($argv[4] >= 18) {
+    echo $argv[3] . " est majeur" . "\n";
+}
+if ($argv[4] < 18) {
+    echo $argv[3] . " est mineur" . "\n";
+}
+
+
+if ($argv[6] >= 18) {
+    echo $argv[5] . " est majeur" . "\n";
+}
+
+if ($argv[6] < 18) {
+    echo $argv[5] . " est mineur" . "\n";
+}
