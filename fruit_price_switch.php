dc6554e3034aecd88c7d962d3ab8c843a9fc5aee
Vagrant Default User <vagrant@billetterie>
2018-06-20 17:05:01 +0000

    Utilisation fonction switch

---
+++ fruit_price_switch.php
@@ -0,0 +1,18 @@
+<?php
+
+switch ($argv[1]) {
+    case "banana":
+    echo $argv[1] . " costs 3$" . "\n";
+    break;
+
+    case "orange":
+    echo $argv[1] . " costs 2$" . "\n";
+    break;
+
+    case "apple":
+    echo $argv[1] . " costs trop cher" . "\n";
+    break;
+
+    default:
+    echo "on ne vend pas de " . $argv[1] . "\n";
+}
