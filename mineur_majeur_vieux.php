66a00c21ae537ad7e2950a60e101e93821bf98c0
Vagrant Default User <vagrant@billetterie>
2018-06-20 15:56:30 +0000

    1ère utilisation de elseif et else

---
+++ mineur_majeur_vieux.php
@@ -0,0 +1,32 @@
+<?php
+if ($argv[2] >= 60) {
+    echo $argv[1] . " a " . $argv[2] . " ans et est vieux" . "\n";
+}
+
+elseif ($argv[2] >= 18 ) {
+    echo $argv[1] . " a " . $argv[2] . " ans et est majeur" . "\n";
+}
+
+else echo $argv[1] . " a " . $argv[2] . " ans et est mineur" . "\n";
+
+if ($argv[4] >= 60) {
+    echo $argv[3] . " a " . $argv[4] . " ans et est vieux" . "\n";
+}
+
+elseif ($argv[3] >= 18 ) {
+    echo $argv[4] . " a " . $argv[4] . " ans et est majeur" . "\n";
+}
+
+else echo $argv[3] . " a " . $argv[4] . " ans et est mineur" . "\n";
+
+if ($argv[6] >= 60) {
+    echo $argv[5] . " a " . $argv[6] . " ans et est vieux" . "\n";
+}
+
+elseif ($argv[6] >= 18 ) {
+    echo $argv[5] . " a " . $argv[6] . " ans et est majeur" . "\n";
+}
+
+else echo $argv[5] . " a " . $argv[6] . " ans et est mineur" . "\n";
+
+
