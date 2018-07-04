d90830f43da1749b5be574330a4a6f17adef4061
Vagrant Default User <vagrant@billetterie>
2018-07-04 16:56:16 +0000

    Prévenir le client qu'il n'a pas précisé de fruit avec isset

--- fruit_price_switch.php
+++ fruit_price_switch.php
@@ -1,18 +1,25 @@
 <?php
 
-switch ($argv[1]) {
-    case "banana":
-    echo $argv[1] . " costs 3$" . "\n";
-    break;
+if (isset($argv[1])) {
 
-    case "orange":
-    echo $argv[1] . " costs 2$" . "\n";
-    break;
+    switch ($argv[1]) {
+        case "banana":
+        echo $argv[1] . " costs 3$" . "\n";
+        break;
 
-    case "apple":
-    echo $argv[1] . " costs trop cher" . "\n";
-    break;
+        case "orange":
+        echo $argv[1] . " costs 2$" . "\n";
+        break;
 
-    default:
-    echo "on ne vend pas de " . $argv[1] . "\n";
+        case "apple":
+        echo $argv[1] . " costs trop cher" . "\n";
+        break;
+
+        default:
+        echo "on ne vend pas de " . $argv[1] . "\n";
+    }
+} else {
+    echo "Précisez un fruit". "\n";
 }
+
+
